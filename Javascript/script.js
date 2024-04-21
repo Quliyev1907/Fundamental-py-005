2f78c5e205d2e014123b59f8a019d3f4becc4646
Quliyev1907 <nvusaln@gmail.com>
2024-04-21 16:56:47 +0400

    practica

---
+++ Javascript/script.js
@@ -0,0 +1,17 @@
+// alert("HELLO VUSAL")
+
+// let cavab= confirm("sen meni basa dusursen?");
+
+// console.log(cavab)
+// if (cavab) { 
+//     alert("sen daxil oldun")
+// } else {
+//     alert("sen daxil olmadin")
+// }
+
+var firstname = prompt("senim adom medor")
+   if(firstname='Vwsal') {
+     alert(`senin adin vsualdoir`);
+   } else {
+    alert(`yox adim vwsal deyul`);
+   }
